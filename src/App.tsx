1540aa37157015457eadfe06659909dd31bdc721
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-10 17:16:24 -0700

    defines types for board and adds state

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,40 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [game, setGame] = useState<Game>({
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
+  type Square = 'X' | 'O' | ' '
+  type Row = [Square, Square, Square, Square, Square, Square, Square, Square]
+  type Board = [Row, Row, Row, Row, Row, Row, Row, Row]
+  type Game = {
+    board: Board
+    id: null | number
+    winner: null | string
+  }
+
   return (
     <div className="App">
-      <h1></h1>
+      <h1>Minesweeper</h1>
+      <ul>
+        {game.board.map(function (row, rowIndex) {
+          return row.map(function (column, columnIndex) {
+            return <li key={column}>{column}</li>
+          })
+        })}
+      </ul>
     </div>
   )
 }
