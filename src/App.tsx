cf9b0ace195b0131f7b87504560c3bcbdb3170da
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-11 10:30:24 -0700

    converts game board mapping to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,61 @@ import React, { useState } from 'react'
 
 function App() {
   const [game, setGame] = useState<Game>({
-    board: [
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ],
+    board: [],
     id: null,
     winner: null,
+    state: null,
   })
 
-  type Cell = ' ' | '_' | 'F' | '*' | '@' | '1'
+  type Cell =
+    | ' '
+    | '_'
+    | 'F'
+    | '*'
+    | '@'
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
   type Row = Array<Cell>
   type Board = Array<Row>
   type Game = {
     board: Board
     id: null | number
+    state: string | null
     winner: null | string
   }
 
-  async function handleNewGame() {
+  async function handleNewGame(difficulty: number) {
     const response = await fetch(
       'https://minesweeper-api.herokuapp.com/games',
       {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ difficulty }),
       }
     )
+    const newGame = (await response.json()) as Game
     if (response.ok) {
-      const newGame = await response.json()
+      setGame(newGame)
+    }
+  }
 
+  async function handleClickCell(row: number, col: number) {
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+    const body = { row, col }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    if (response.ok) {
+      const newGame = (await response.json()) as Game
       setGame(newGame)
     }
   }
@@ -44,14 +65,21 @@ function App() {
     <div>
       <h1>Minesweeper</h1>
       <div className="button">
-        <button onClick={handleNewGame}>Start a game!</button>
+        <button onClick={() => handleNewGame(0)}>Start a game!</button>
       </div>
       <ul>
-        {game.board.map(function (row, rowIndex) {
-          return row.map(function (column, columnIndex) {
-            return <li key={column}>{column}</li>
-          })
-        })}
+        {game.board.map((row, rowIndex) =>
+          row.map((column, columnIndex) => (
+            <li
+              onClick={() => {
+                handleClickCell(rowIndex, columnIndex)
+              }}
+              key={column}
+            >
+              {column}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   )
